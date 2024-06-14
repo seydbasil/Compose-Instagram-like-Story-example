91b509c09d8e24118a25d071c5cae9a5585acfa1
seydbasil <seyd.mb@accubits.com>
2024-06-14 19:30:24 +0530

    layout changes

--- app/src/main/java/com/smbvt/bst/reels/ui/screens/ReelsScreen.kt
+++ app/src/main/java/com/smbvt/bst/reels/ui/screens/ReelsScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -22,49 +21,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.smbvt.bst.reels.R
-import com.smbvt.bst.reels.models.ReelsModel
 import com.smbvt.bst.reels.ui.composables.LineIndicatorRow
 import com.smbvt.bst.reels.ui.theme.PaddingDefault24
 import com.smbvt.bst.reels.utils.Utils
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ReelsScreen(
     modifier: Modifier = Modifier,
     onClickClose: () -> Unit = {},
 ) {
-    Box(
-        modifier = modifier.fillMaxSize()
-            .statusBarsPadding()
-    ) {
-        // Here we can add common background for all stories
 
-//        Image(
-//            painter = painterResource(id = R.drawable.bg_explainer),
-//            contentDescription = "splashbg",
-//            modifier = Modifier.fillMaxSize(),
-//            contentScale = ContentScale.Crop,
-//            alignment = Alignment.Center
-//        )
+    val listOfImage = Utils.getContent()
 
-        StoryScreen(
-            Utils.getContent(), onClickClose = onClickClose
-        )
-    }
-}
-
-@Preview
-@Composable
-fun PreviewReelsScreen() {
-    ReelsScreen()
-}
-
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun StoryScreen(listOfImage: List<ReelsModel>, onClickClose: () -> Unit) {
-
-    val pagerState = rememberPagerState(initialPage = 0,
+    val pagerState = rememberPagerState(
+        initialPage = 0,
         initialPageOffsetFraction = 0f,
         pageCount = { listOfImage.size })
     val coroutineScope = rememberCoroutineScope()
@@ -94,51 +66,54 @@ fun StoryScreen(listOfImage: List<ReelsModel>, onClickClose: () -> Unit) {
     }
 
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .statusBarsPadding()
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = PaddingDefault24)
-        ) {
-            LineIndicatorRow(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = PaddingDefault24),
-                isPaused = isPaused,
-                currentPage = currentPage,
-                onClickClose = onClickClose,
-                moveToNext = {
-                    moveToNext()
-                },
-                listOfImage = listOfImage.size
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1f),
-            ) {
-                var pagePosition by remember {
-                    mutableIntStateOf(0)
-                }
-                StoryPager(modifier = Modifier.fillMaxSize(),
-                    pagerState = pagerState,
-                    listOfImage = listOfImage,
-                    onClickLeft = {
-                        moveToPrevious()
-                    },
-                    onCLickRight = {
-                        moveToNext()
-                    },
-                    onPause = {
-                        isPaused = true
-                    },
-                    onResume = {
-                        isPaused = false
-                    },
-                    onChangePage = { pagePosition = it })
-            }
+        // Here we can add common background for all stories
+
+//        Image(
+//            painter = painterResource(id = R.drawable.bg_explainer),
+//            contentDescription = "splashbg",
+//            modifier = Modifier.fillMaxSize(),
+//            contentScale = ContentScale.Crop,
+//            alignment = Alignment.Center
+//        )
+
+        var pagePosition by remember {
+            mutableIntStateOf(0)
         }
+        StoryPager(modifier = Modifier.fillMaxSize(),
+            pagerState = pagerState,
+            listOfImage = listOfImage,
+            onClickLeft = {
+                moveToPrevious()
+            },
+            onCLickRight = {
+                moveToNext()
+            },
+            onPause = {
+                isPaused = true
+            },
+            onResume = {
+                isPaused = false
+            },
+            onChangePage = { pagePosition = it })
+
+        LineIndicatorRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+                .padding(horizontal = PaddingDefault24),
+            isPaused = isPaused,
+            currentPage = currentPage,
+            onClickClose = onClickClose,
+            moveToNext = {
+                moveToNext()
+            },
+            listOfImage = listOfImage.size
+        )
+
         Image(painter = painterResource(id = R.drawable.ic_close_reels),
             contentDescription = null,
             modifier = Modifier
@@ -148,5 +123,10 @@ fun StoryScreen(listOfImage: List<ReelsModel>, onClickClose: () -> Unit) {
                     onClickClose()
                 })
     }
+}
 
+@Preview
+@Composable
+fun PreviewReelsScreen() {
+    ReelsScreen()
 }
