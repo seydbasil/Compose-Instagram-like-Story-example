931fe9b4074dc7d0a9c5820ab6d778776ee31ee4
seydbasil <seyd.mb@accubits.com>
2024-06-21 09:03:38 +0530

    full screen UI

--- app/src/main/java/com/smbvt/bst/reels/MainActivity.kt
+++ app/src/main/java/com/smbvt/bst/reels/MainActivity.kt
@@ -1,28 +1,40 @@
 package com.smbvt.bst.reels
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.smbvt.bst.reels.ui.navigation.StoryNavHost
 import com.smbvt.bst.reels.ui.theme.ComposeInstagramLikeReelsExampleTheme
+import com.smbvt.bst.reels.utils.hideSystemUI
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ComposeInstagramLikeReelsExampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().systemBarsPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     StoryNavHost()
                 }
             }
         }
+
+        hideSystemUI(this)
+
     }
 }
